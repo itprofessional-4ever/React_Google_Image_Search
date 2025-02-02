974dd885827c0892febc5c13c5f1500875bf1e4b
Chloe Lee <itprofessional.4ever7@gmail.com>
2025-02-02 14:45:23 -0700

    abc

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const searchImages = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
-      Authorization: 'Client-ID ebd-CfbTMQzq0waQ9AyM33FsCjPO5PgNuRu_WlCs0dg'
+      Authorization: 'Client-ID '
     },
     params: {
       query: 'term',
